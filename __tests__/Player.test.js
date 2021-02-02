e83e9d9dbda32934cacd68b6a5e5a7e5ae414dd1
Shanshan Xu <tinaxu84@gmail.com>
2021-02-02 15:07:25 -0500

    player

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -9,7 +9,7 @@ jest.mock('../lib/Potion.js');
 
 console.log(new Potion());
 
-
+// test player object
 test('create a player object', () =>{
     const player = new Player('Dave');
 
@@ -18,4 +18,25 @@ test('create a player object', () =>{
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
-})
+});
+
+// add test to get the stats about the player
+test ("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+// add test to get the inventory stats
+test ('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+
+    player.inventory = [];
+
+    expect(player.getInventory()).toEqual(false);
+});
